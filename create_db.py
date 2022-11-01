2f55b19346b9ba90864178bf5c3b39ebdaaebe61
Marcelo Sanches <sanches.cello@gmail.com>
2022-10-31 22:57:30 -0600

    Create table first then stage transformed table

--- create_db.py
+++ create_db.py
@@ -2,15 +2,19 @@ import duckdb
 conn = duckdb.connect('./data/books.duckdb')
 
 cursor = conn.cursor()
+
 cursor.execute("""
     CREATE SCHEMA IF NOT EXISTS core;
-    CREATE TABLE IF NOT EXISTS core.raw_amzn_books_data(
-        title VARCHAR
-    );
-"""
-)
+""")
 
-print(cursor.execute('DESCRIBE core.raw_amzn_books_data;').fetchall())
+cursor.execute("""
+    CREATE OR REPLACE TABLE core.raw_amzn_books_data AS 
+        SELECT * FROM read_csv_auto(
+            'book_reviews/core/raw_amzn_books_data.csv'
+        );
+""")
+
+#print(cursor.execute('DESCRIBE core.raw_amzn_books_data;').fetchall())
 
 cursor.close()
 conn.close()
