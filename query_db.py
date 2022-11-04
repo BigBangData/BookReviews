507f0727cdb7c61d7dbb2762cd90f20bd8c1fe41
Marcelo Sanches <sanches.cello@gmail.com>
2022-11-03 19:33:54 -0600

    Test some queries

--- query_db.py
+++ query_db.py
@@ -4,7 +4,8 @@ conn = duckdb.connect('./data/books.duckdb')
 
 cursor = conn.cursor()
 pragma = cursor.execute("""
-    PRAGMA table_info('core.stg_amzn_books_rating');
+    PRAGMA show_tables;
+    PRAGMA table_info('core.vw_amzn_books_ratings');
 """
 )
 
@@ -15,7 +16,8 @@ for ix, col in enumerate(res1):
     print([(ix+1, val) for ct, val in enumerate(col) if ct == 1])
 
 # query = cursor.execute("""
-#     SELECT *
+#     SELECT * lower = 2703963, 2667782
+#     2991226 distinct
 #     FROM core.stg_amzn_books_rating
 #     LIMIT 5;
 # """)
@@ -23,7 +25,22 @@ for ix, col in enumerate(res1):
 # res2 = query.fetchall()
 # print(res2)
 
-print(cursor.execute('SELECT COUNT(*) FROM core.stg_amzn_books_rating;').fetchall())
+q = cursor.execute("""
+    SELECT *
+    FROM core.vw_amzn_books_ratings
+    ORDER BY title
+    LIMIT 50;
+""")
+print(q.fetchall())
+
+# q2 = cursor.execute("""
+#     SELECT COUNT(*) AS CTD
+#     FROM (
+#         SELECT DISTINCT * FROM core.vw_amzn_books_ratings
+#     );
+# """)
+# print(q2.fetchall())
+
 # for ix, row in enumerate(res2):
 #     print('Row', ix+1,': ',
 #         [val[:15] for ct, val in enumerate(row) if val]
